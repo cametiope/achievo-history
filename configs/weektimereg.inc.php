1f1bf7bda6b935edb7e036fac958950e84cbc4e6
Guido (SVN Import) <info@achievo.org>
2007-05-11 16:00:23 +0000

    + Added aid weektimereg config. [Guido]

---
+++ configs/weektimereg.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+  // Use this setting to descide what projects to display. Can
+  // be either 'recent' to display the recent projects (default)
+  // or 'byteam' to display the projects by team (and project 
+  // startdate and enddate)
+  // or 'projectlist' - to display projects only from users list
+  // (in this case we must set autoshowactiveprojects to false too.
+  $config["project_selection"] = "projectlist";
+  
+  // When the setting above is 'by team', set the id of a default 
+  // activity to display. Other activities can be added with an 
+  // automatically added set of selectboxes.
+  // Set to 0 or remove config to fallback to default functionality.
+  // $config["project_selection_byteam_activityfilter"]  = 1;
+  
+  // Set to 'true' to hide the 'add project/phase/activity' link
+  $config["hide_add_project_link"] = false;
+
+  // If set to true, active projects do not need to have "Always
+  // visible in time registration" to be set.
+  $config["autoshowactiveprojects"] = false;
+  
+  // If set to true, customer column will be show on weektimereg
+  // and select_projectselect pages
+  $config["showOrganization"] = false;
+?>
