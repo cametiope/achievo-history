47a0dec34dfeb0831c11a0f5e9664227716b4df7
Golovnya Yury (SVN Import) <info@achievo.org>
2008-06-18 21:50:17 +0000

    "Add new configuration options"

--- configs/project.inc.php
+++ configs/project.inc.php
@@ -2,5 +2,17 @@
 
   // This variable configures wether hour registrations should be automatically deleted when
   // a phase is deleted. If omitted, false is default.
-  $config["project_cascading_delete_hours"] = false; 
+  $config["project_cascading_delete_hours"] = false;
+
+  //Option to hide/disabled the packages.
+  $config["package_enabled"] = true;
+
+  //The limit of branches for sub_packages. By default this value is -1(unlimited).
+  $config["package_number_of_branches"] = -1; 
+
+
+  //For backwards compatibility issues, the subprojects will still be available in Achievo. 
+  //The functionality of subprojects can be turned off.
+  $config["use_subprojects"] = true;   
+
 ?>
